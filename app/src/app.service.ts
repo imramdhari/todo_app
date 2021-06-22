e3abbc2345c150313fe9cd03ec9626fcf21a1369
Balvinder Singh <53423052+imramdhari@users.noreply.github.com>
2021-06-22 16:59:43 +1000

    Update app.service.ts

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World I love you!';
+    return 'Hello World';
   }
 }
